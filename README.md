691202c0ea7bc64a672e81c5c583190c7047baa5
Jori <jori.gelbaugh@atomicobject.com>
2021-09-12 14:38:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# About
+A one page portfolio website, developed using a base template with styling from Milan Savov. I reworked parts of the HTML, CSS, and JavaScript and added on to the site.
+
+## Acknowledgements
+Credit goes to Milan Savov (milan.savov@gmail.com) for their <a href="https://github.com/msavov/html-freebie-portfolio-perfect">template</a>. Credit for developing the <a href="https://schier.co/blog/simple-vanilla-javascript-typing-carousel">typing carousel</a>  animation goes to <a href="https://codepen.io/bpanicker13">Binu Panicker</a>.
---
+++ js/script.js
@@ -0,0 +1,113 @@
+$(function() {
+
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(document).on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.site-header',
+        offset: 10
+    });
+
+	/* Progress bar */
+    var $section = $('.section-skills');
+    function loadBars() {
+	    $('.progress .progress-bar').progressbar({
+	        transition_delay: 500
+	    });
+    }
+    
+    $(document).bind('scroll', function(ev) {
+        var scrollOffset = $(document).scrollTop();
+        var containerOffset = $section.offset().top - window.innerHeight;
+        if (scrollOffset > containerOffset) {
+            loadBars();
+            // unbind event not to load scrolsl again
+            $(document).unbind('scroll');
+        }
+    });
+
+    // Navigation overlay
+    var s = skrollr.init({
+            forceHeight: false,
+            smoothScrolling: false,
+            mobileDeceleration: 0.004,
+            mobileCheck: function() {
+                //hack - forces mobile version to be off
+                return false;
+            }
+    });
+
+    // Typing effect
+    var TxtRotate = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtRotate.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 300 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        setTimeout(function() {
+            that.tick();
+        }, delta);
+    };
+
+    $(document).ready(function () {
+        var elements = document.getElementsByClassName('txt-rotate');
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-rotate');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtRotate(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #fff }";
+        document.body.appendChild(css);
+    });
+
+    var range_el = document.querySelector('input');
+
+    range_el.addEventListener('change', function() {
+    this.setAttribute('value', this.value);
+    }, false);
+
+    range_el.addEventListener('input', function() {
+    this.setAttribute('value', this.value);
+    }, false);
+});
